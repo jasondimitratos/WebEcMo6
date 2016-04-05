3654a34bc5ffca927176964130f88ea7478e2b63
Jason Dimitratos <jason.dimitratos@students.fhnw.ch>
2016-04-05 19:36:44 +0200

    viele veränderungen aussehen und functionalität auf die verschiedene buttons

---
+++ Javascript/myscript.js
@@ -0,0 +1,30 @@
+$('document').ready(function(){
+    $('#where').hide();
+    $('#who').hide();
+    $('#what').find('button').on('click',function(event){
+        event.preventDefault();
+
+        $('#what').hide();
+        $('#where').show();
+    });
+    /**Diese Function ist ein eventhandler für den footer die Das entsprechende element anzeigt*/
+    $('nav').on('click','button',function(event){
+        event.preventDefault();
+
+        var ButtonName=$(this).text();
+        ButtonName=ButtonName.toLowerCase();
+        ButtonName=ButtonName.replace(" ", "");
+
+        $('main').children().each(function(){
+            if($(this).attr('id')===ButtonName){
+                    $(this).show();
+            }else{
+                $(this).hide();
+            }
+        });
+
+
+
+    });
+
+});
